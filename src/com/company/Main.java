b4a96a469f63ced5fa1d02a4c5185868eb8ff5fc
gorki <gorkiy17@gmail.com>
2020-08-23 18:37:24 +0300

    1

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = a + b;
+        if (c % 2 == 0 || (a += 2) % 2 == 0) {
+            System.out.println("четное " + a);
+        } else {
+            System.out.println("нечетное " + a);
+        }
+        //System.out.println(c);
+    }
+}
